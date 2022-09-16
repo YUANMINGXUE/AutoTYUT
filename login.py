7c1db991ea7f1b4e2fdbc4763c7c599d034edaa5
“wuyunhao” <wuyunhaol@163.com>
2022-09-16 13:31:34 +0800

    switch window

--- login.py
+++ login.py
@@ -14,8 +14,8 @@ def login(username, password):
     option = webdriver.EdgeOptions()
     option.add_experimental_option(name='excludeSwitches', value=['enable-automation'])
     option.add_experimental_option('useAutomationExtension', False)
-    option.add_argument(
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.53')
+    # option.add_argument(
+    #     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.53')
 
     # 静默运行，不建议使用
     # option.add_argument('headless')
@@ -32,8 +32,17 @@ def login(username, password):
     driver.find_element(By.ID, "btnSubmit").click()
     # 让这家伙睡会  O(∩_∩)O哈哈~  为什么要睡呢，这是为了模拟手动登录，防止被封
     time.sleep(3)
+
+    wins = driver.window_handles
+
+    handle = driver.current_window_handle
+    driver.maximize_window()
     # 循环点击问卷
     while (len(driver.find_elements(By.PARTIAL_LINK_TEXT, '全体研究生健康状况报告'))):
+        # 切换窗口
+        driver.switch_to.window(handle)
+        driver.maximize_window()
+        time.sleep(1.25)
         # 选择上午/下午
         driver.find_elements(By.PARTIAL_LINK_TEXT, '全体研究生健康状况报告')[0].click()
         time.sleep(3)
@@ -42,8 +51,12 @@ def login(username, password):
         driver.find_element(By.XPATH, "//*[@id='div4']/div[2]/label").click()
         time.sleep(6)  # 等待定位，等久一点
 
+        # 滚动到底部
+        # driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        # time.sleep(0.75)
         # 勾选承诺真实
-        driver.find_element(By.XPATH, "//*[@id='div16']/div[2]/div/div").click()
+        # driver.find_element(By.XPATH, "//*[@id='div16']/div[2]/div/div").click()
+        driver.find_element(By.XPATH, "//*[@id='div16']/div[2]/div").click()
 
         time.sleep(1)
         # 玄学，勿删
--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from login import login
 if __name__ == '__main__':
     # 用户列表
     userlist = [
-        ('学号', '密码'),
-        ('学号', '密码'),
-        ('学号', '密码'),
+        # ('学号', '密码'),
+        # ('学号', '密码'),
+        # ('学号', '密码'),
+
     ]
     for i in userlist:
         login(i[0], i[1], )
